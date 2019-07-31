30a1f5d1c1552de9bda37b7e97bde09f246b7e36
Leandro Lavore <leolavore@gmail.com>
2019-07-31 16:52:52 +1000

    linked

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,8 +9,8 @@ export default function Home(props) {
             <nav className="mobnav"><p className="mobtitle">Mobile-shop</p></nav>
 
             <div className="links">
-            <a><img style={{height: "40px", width: "40px"}} src={require("../links/git-white.png")}/></a>
-            <a><img style={{height: "40px", width: "40px"}} src={require("../links/linkedin.ico")}/></a>
+            <a href="https://github.com/leandrolavore"><img style={{height: "40px", width: "40px"}} src={require("../links/git-white.png")}/></a>
+            <a href="https://www.linkedin.com/in/leandrolavore/"><img style={{height: "40px", width: "40px"}} src={require("../links/linkedin.ico")}/></a>
             </div>
             <ProductListing product={data.products}/>
         </div>
